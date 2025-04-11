652f8f608526accafa1013af3e46c6ee932bde3a
JuanCrTo <jc4977241@gmail.com>
2025-04-11 13:37:52 -0500

    feat: :sparkles: Agregar interface y schema para el modelo de usuario

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+import { UserRole } from "../enums/user-role.enum";
+
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  createdAt: Date;
+  updatedAt: Date;
+}
---
+++ src/user/model/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { UserRole, USER_ROLES } from '../enums/user-role.enum';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true, versionKey: false })
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true, unique: true })
+  password: string;
+
+  @Prop({ required: true, enum: USER_ROLES })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
