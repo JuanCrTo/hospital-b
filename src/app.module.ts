ecef1e26c964da3e0b54f474e5de9742bbbde676
JuanCrTo <jc4977241@gmail.com>
2025-04-10 21:36:41 -0500

    fix: :sparkles: Generar recurso inicial de autenticación

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
 import { UserModule } from './user/user.module';
-import { PacienteModule } from './paciente/paciente.module';
-import { EnfermeroModule } from './enfermero/enfermero.module';
-import { CitaModule } from './citas/cita.module';
-import { HistorialModule } from './historial/historial.module';
-import { MedicamentoModule } from './medicamento/medicamento.module';
 import { NurseModule } from './nurse/nurse.module';
 import { PatientModule } from './patient/patient.module';
 import { AppointmentModule } from './appointment/appointment.module';
 import { MedicalRecordModule } from './medical-record/medical-record.module';
 import { MedicineModule } from './medicine/medicine.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DoctorModule, UserModule, PacienteModule, EnfermeroModule, CitaModule, HistorialModule, MedicamentoModule, NurseModule, PatientModule, AppointmentModule, MedicalRecordModule, MedicineModule],
+  imports: [DoctorModule, UserModule, NurseModule, PatientModule, AppointmentModule, MedicalRecordModule, MedicineModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
