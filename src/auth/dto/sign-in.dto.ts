40521cd0c28d035243189b5d0e4a4f021e949691
JuanCrTo <jc4977241@gmail.com>
2025-04-17 11:30:26 -0500

    feat: :sparkles: Añadir validación de SignInDto y crear interfaz IJwtPayload

---
+++ src/auth/dto/sign-in.dto.ts
@@ -0,0 +1,33 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class SignInDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString({ message: 'Email must be a string' })
+  @IsEmail(
+    { allow_display_name: false, require_tld: true },
+    { message: 'Invalid email address' },
+  )
+  email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString({ message: 'Password must be a string' })
+  @IsStrongPassword(
+    {
+      minLength: 6,
+      minUppercase: 1,
+      minLowercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol',
+    },
+  )
+  password: string;
+}
