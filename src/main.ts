1aa9f62eef84c02c854d337f0aa2c6cfcdb543e6
JuanCrTo <jc4977241@gmail.com>
2025-05-22 15:16:15 -0500

    feat: :wrench: Configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const configService = app.get(ConfigService);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  const configService = app.get(ConfigService)
+
   app.enableCors({
-    origin: [
-      configService.get<string>('LOCAL_URL_2'),
-      configService.get<string>('LOCAL_URL'),
-    ],
+    origin: [configService.get<string>('LOCAL_URL_2'), configService.get<string>('LOCAL_URL')],
     methods: 'GET,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(configService.get<number>('PORT'));
+    credentials: true
+  })
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Hospital Yellow API').setDescription('API para el sistema de gestión hospitalaria').setVersion('1.0').addBearerAuth().build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(configService.get<number>('PORT'))
 }
-bootstrap();
+bootstrap()
